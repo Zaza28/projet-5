60097f34ab021ec0887daa2d09367760030f21dd
Zaza28 <zainebharzi26@gmail.com>
2024-01-12 23:48:06 +0100

    add all ajout de tout mon travail de la semaine, carousel pas encore terminé

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,124 @@
-// clique sur les flèches de direction 
-
-let flèchesDirectionLeft = document.querySelector(".arrow_left");
-
-flèchesDirectionLeft.addEventListener("click", function(){
-	console.log("vous avez cliqué sur la flèche gauche")
-});
-
-let flèchesDirectionRight = document.querySelector(".arrow_right");
-
-flèchesDirectionRight.addEventListener("click", function(){
-	console.log("vous avez cliqué sur la flèche droite")
-});
-
 //carrousel
 const slides = [
-
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-
 ]
 
-//points de carrousel
+const banner = document.getElementById("banner");
+let leftArrow = document.querySelector(".arrow_left");
+let rightArrow = document.querySelector(".arrow_right");
+
+leftArrow.addEventListener("click", function () {
+	console.log("A gauche");
+
+});
+
+
+
+rightArrow.addEventListener ("click", function () {
+	console.log("A droite");
+});
+
+
+
+function addSpan() {
+
+	document.querySelector(".dots").innerHTML += "<span class='dot'></span>";
+	document.querySelector(".dots").innerHTML += "<span class='dot'></span>";
+	document.querySelector(".dots").innerHTML += "<span class='dot'></span>";
+	document.querySelector(".dots").innerHTML += "<span class='dot'></span>";
+
+}
+addSpan();
+document.querySelector(".dot").classList.add("dot_selected");
+
+let Index = 0;
+
+for (let i = 0; i < slides.length; i++) {
+
+	let slide = slides[i];
+
+	if (i <= slides.length) {
+
+		let img = document.querySelector(".banner-img");
+
+		img.setAttribute("src", "./assets/images/slideshow/slide1.jpg");
+		i++;
+
+
+	}
+	if (i <= slides.length) {
+		let img = document.querySelector(".banner-img");
+		img.setAttribute("src", "./assets/images/slideshow/slide2.jpg");
+		i++;
+
+
+
+
+	}
+	if (i <= slides.length) {
+		let img = document.querySelector(".banner-img");
+		img.setAttribute("src", "./assets/images/slideshow/slide3.jpg");
+		i++;
+
+
+	}
+
+	if (i === slides.length[3]) {
+		let img = document.querySelector(".banner-img");
+		img.setAttribute("src", "./assets/images/slideshow/slide4.jpg");
+
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
