16cdc3d37c2ba6909e3a8481a5f00e205499c30c
Zaza28 <zainebharzi26@gmail.com>
2024-01-22 17:12:30 +0100

    add all modification de la partie des points séléctionnés

--- assets/script.js
+++ assets/script.js
@@ -1,85 +1,77 @@
-//carrousel
 const slides = [
-	{
-		"image": "slide1.jpg",
-		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image": "slide2.jpg",
-		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image": "slide3.jpg",
-		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image": "slide4.png",
-		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 const banner = document.getElementById("banner");
 let leftArrow = document.querySelector(".arrow_left");
 let rightArrow = document.querySelector(".arrow_right");
+const tagLine = document.querySelector("#banner p");
+const image = document.querySelector(".banner-img");
+const indexParent = document.querySelector("#banner .dots ");
 let index = 0;
 
 leftArrow.addEventListener("click", function () {
-	index--;
-	if (index === -1) {
-		index = 3;
-		
-
-	}
-	console.log(slides[index]);
-	tagLine.innerHTML = slides[index].tagLine;
-	image.setAttribute("src", "./assets/images/slideshow/" + slides[index].image);
-	indexParent.children[index].classList.add("dot_selected");
-
-
+  index--;
+  if (index === -1) {
+    index = slides.length - 1;
+  }
+  displaySlide(index);
 });
 
 rightArrow.addEventListener("click", function () {
-	index = index + 1;
-	console.log(slides[index])
-	if (index === 4) {
-		index = 0;
-	}
-	indexParent.children[index].classList.add("dot_selected");
-	tagLine.innerHTML = slides[index].tagLine;
-	image.setAttribute("src", "./assets/images/slideshow/" + slides[index].image);
-
+  index = index + 1;
+  console.log(slides[index]);
+  if (index === slides.length) {
+    index = 0;
+  }
+  displaySlide(index);
 });
 
-const dotsSelected = document.querySelectorAll(".dot")
-const tagLine = document.querySelector('#banner p');
-const image = document.querySelector('.banner-img');
-const indexParent = document.querySelector("#banner .dots ")
-
 function addSpan() {
-	document.querySelector(".dots").innerHTML += "<span class='dot'></span>";
-	document.querySelector(".dots").innerHTML += "<span class='dot'></span>";
-	document.querySelector(".dots").innerHTML += "<span class='dot'></span>";
-	document.querySelector(".dots").innerHTML += "<span class='dot'></span>";
+  for (let i = 0; i < slides.length; i++) {
+    document.querySelector(".dots").innerHTML += "<span class='dot'></span>";
+  }
+  const listdot = document.querySelectorAll(".dot");
 
-	const listdot = document.querySelectorAll(".dot")
-	console.log(listdot);
-
-	listdot.forEach((element, i) => {
-
-		element.addEventListener("click", () => { 
-			index = i;
-			element.classList.add("dot_selected");
-			image.setAttribute("src", "./assets/images/slideshow/" + slides[i].image);
-			tagLine.innerHTML = slides[i].tagLine;
-
-		});
-		// element.classList.remove("dot_selected");
-
-
-
-
-	});
+  listdot.forEach((element, i) => {
+    element.addEventListener("click", () => {
+      index = i;
+      displaySlide(index);
+    });
+  });
 }
 
 addSpan();
 
+function displaySlide(newIndex) {
 
+  tagLine.innerHTML = slides[newIndex].tagLine;
+  image.setAttribute( "src", "./assets/images/slideshow/" + slides[newIndex].image );
+  indexParent.children[newIndex].classList.add("dot_selected");
+
+  Array.from(indexParent.children).forEach((element, i) => {
+    if (i === newIndex) {
+      element.classList.add("dot_selected");
+    } else {
+      element.classList.remove("dot_selected");
+    }
+  });
+
+}
+
+displaySlide(0);
